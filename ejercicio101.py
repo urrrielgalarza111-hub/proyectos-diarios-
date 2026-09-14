b092ac0d4f58d0664a5ac1793ca6449b39e6fd91
Uriel Antonio Galarza <urrrielgalarza111-hub>
2026-09-14 13:46:39 -0300

    dia30(14/09/26)

---
+++ ejercicio101.py
@@ -0,0 +1,18 @@
+#Desarrollar una funcion que reciba un string como parametro y
+# nos muestre la cantidad de vocales.
+# Llamarla desde el bloque principal del programa 3 veces con string distintos.
+
+def cant_vocales(cadena):
+    cant=0
+    for x in range(len(cadena)):
+        if cadena[x]=="a" or cadena[x]=="e" or  cadena[x]=="i" or cadena[x]=="o" or cadena[x]=="u":
+            cant=cant+1
+    
+    print(cadena,"tiene :", cant,"vocales")
+            
+
+#bloque principal
+
+cant_vocales("jugar")
+cant_vocales("correr")
+cant_vocales("estudiar")
